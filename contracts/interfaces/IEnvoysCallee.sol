8824f60614ef2dcee5e346a65a3202ff80703230
Sergey <63466627+xsubject@users.noreply.github.com>
2021-09-27 16:59:00 +0300

    Update IEnvoysCallee.sol

--- contracts/interfaces/IEnvoysCallee.sol
+++ contracts/interfaces/IEnvoysCallee.sol
@@ -1,5 +1,5 @@
 pragma solidity >=0.5.0;
 
 interface IEnvoysCallee {
-    function EnvoysCall(address sender, uint amount0, uint amount1, bytes calldata data) external;
+    function envoysCall(address sender, uint amount0, uint amount1, bytes calldata data) external;
 }
